5f2eb9da7dce7eabedc1ed9d0ccfdfd220713f34
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-13 11:57:35 -0500

    finishing the country picker and chart module

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import './App.css';
-import {Cards, Chart, CountryPicker} from './components';
-import styles from './App.module.css';
-import {fetchData} from './api';
+import React from "react";
+import "./App.css";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
-class App extends React.Component{
+import websiteLogo from "./images/logo.png";
+
+class App extends React.Component {
   state = {
     data: {},
-    country: '',
-  }
+    country: "",
+  };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    this.setState({ data: fetchedData, country: country })
+    this.setState({ data: fetchedData, country: country });
     // console.log(fetchedData)
   }
 
   handleCountryChange = async (country) => {
     const fetchData = await fetchedData();
 
-    if(country) {
-      changeableUrl = `${url}/countries/${country}`
+    if (country) {
+      changeableUrl = `${url}/countries/${country}`;
     }
-  }
+  };
 
   render() {
     const { data, country } = this.state;
-    
-    return(
+
+    return (
       <div className={styles.container}>
-        <Cards data={this.state.data}/>
-        <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Chart data={data} country={country}/>
+        <img className={styles.images} src={websiteLogo} />
+        <Cards data={this.state.data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
-    )
+    );
   }
 }
 
