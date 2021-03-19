6ba60a7a61c008ccc7b10760fa9060d889fa3646
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-18 22:43:02 -0400

    adding gh-pages

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,10 @@ const url = "https://covid19.mathdro.id/api";
 export const fetchData = async (country) => {
   let changeableUrl = url;
 
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
@@ -21,12 +25,26 @@ export const fetchData = async (country) => {
   }
 };
 
+// export const fetchDailyData = async () => {
+//   try {
+//     const { data } = await axios.get(`${url}/daily`);
+
+//     return data.map(({ confirmed, deaths, reportDate: date }) => ({
+//       confirmed: confirmed.total,
+//       deaths: deaths.total,
+//       date,
+//     }));
+//   } catch (error) {
+//     return error;
+//   }
+// };
+
 export const fetchDailyData = async () => {
   try {
-    const { data } = await axios.get(`${url}/daily`);
+    const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
 
-    return data.map(({ confirmed, deaths, reportDate: date})=> ({ confirmed: confirmed.total, deaths: deaths.total, date}));
-  } catch (error) {
+    return data.map(({ positive, recovered, death, dateChecked: date}) => ({ confirmed: positive, recovered, deaths: death, date}));
+  } catch (error){
     return error;
   }
 };
